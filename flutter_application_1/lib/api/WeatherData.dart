8f7b26f3da4fc41a1f6c2462431b1001ab90a50a
Pankaj Rajani <Pankaj.Rajani@penske.com>
2021-11-12 16:19:40 +0530

    Added the comments in the Weather Data file

--- flutter_application_1/lib/api/WeatherData.dart
+++ flutter_application_1/lib/api/WeatherData.dart
@@ -5,8 +5,12 @@ class WeatherData {
   final String path = "/data/2.5/weather";
   final String apiKey = "2c2f7d29672d1b2bc730b503f24e416d";
 
+  //using async await function to get the data from the API asynchronously
   Future<String> getWeather(String location) async {
-    Map<String, dynamic> paramters = {'q': location, 'appId': apiKey};
+    Map<String, dynamic> paramters = {
+      'q': location,
+      'appId': apiKey
+    }; // query parameters
     Uri uri = Uri.https(baseURI, path, paramters);
     http.Response result = await http.get(uri);
     return result.body;
